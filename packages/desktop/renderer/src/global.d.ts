c034f877982d30641c46aa24f1dbec5a5b15f463
wangfengzhou <luke942021@163.com>
2026-08-06 13:00:50 +0800

    Revert "Merge remote-tracking branch 'origin/role-c/desktop-operations'"
    This reverts commit a51c43343876b51030201499266cc914c0c57235, reversing
    changes made to 0d37b624b4d17c25fa6cdd4963b206a1679b7f5b.

--- packages/desktop/renderer/src/global.d.ts
+++
@@ -1,9 +0,0 @@
-import type { CodentumDesktopBridge } from "@desktop/data/desktop-bridge";
-
-declare global {
-  interface Window {
-    readonly codentum: CodentumDesktopBridge;
-  }
-}
-
-export {};
